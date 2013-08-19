428d707ec44777aa5537517063f2316e5ea61772
mkukar <mkukar@gmail.com>
2013-08-18 21:49:21 -0700

    updated boot.sh

--- Programming/boot.sh
+++ Programming/boot.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-#Boot file to run when Rasberry Pi boots
+# /etc/init.d/myboot.sh
+
+### BEGIN INIT INFO
+# Provides:          RaspiRadio_Boot
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Sets up RaspiRadio at boot
+# Description:       Simple script that sets up the RaspiRadio at boot
+### END INIT INFO
+
+#MOVE THIS FILE TO /etc/init.d/ FOR IT TO WORK ON BOOT
+#RENAME TO myboot.sh
 
 #writes to LCD for easy SSH
 python ~/RaspiRadio/Programming/writelcd.py 'Raspi Radio v1.0' 10.0.1.47
